bba9e18a8efada2cc6ad7d8978c15866183ad918
Ebonne Cabarrus <42730922+ebonnecab@users.noreply.github.com>
2020-07-07 13:50:11 -0700

    created empty df with desired cols

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ credentials = oauth2.SpotifyClientCredentials(
     client_secret=config.client_secret
 )
 
-token = credentials.get_access_token()
+token = credentials.get_cached_token()
 spoty = spotipy.Spotify(auth=token)
 
 #accessing popular spotify playlists w/ username and playlist id
@@ -18,3 +18,14 @@ us_top_fifty = spoty.user_playlist_tracks('spotify', '37i9dQZEVXbLRQDuF5jeBp')
 global_top_fifty = spoty.user_playlist_tracks('spotify', '37i9dQZEVXbMDoHDwVN2tF')
 us_viral_fifty = spoty.user_playlist_tracks('spotify', '37i9dQZEVXbKuaTI1Z1Afx')
 global_viral_fifty = spoty.user_playlist_tracks('spotify', '37i9dQZEVXbLiRSasKsNU9')
+
+#create dataframe from playlists for analysis
+def create_df(playlist):
+    playlist_features = [
+        'artist', 'album', 'track_name', 'track_id', 'key', 
+        'danceability', 'energy', 'instrumentalness', 'loudness',
+         'valence', 'type']
+    
+    playlist_df = pd.DataFrame(columns=playlist_features)
+
+    return playlist_df
