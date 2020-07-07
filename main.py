55abfefaa66461526111d560e2639f2793261614
Ebonne Cabarrus <42730922+ebonnecab@users.noreply.github.com>
2020-07-07 13:30:56 -0700

    added code for spotify auth

---
+++ main.py
@@ -0,0 +1,20 @@
+import config
+import spotipy
+import spotipy.oauth2 as oauth2
+
+#script for Spotify authorization
+credentials = oauth2.SpotifyClientCredentials(
+    client_id=config.client_id,
+    client_secret=config.client_secret
+)
+
+token = credentials.get_access_token()
+spoty = spotipy.Spotify(auth=token)
+
+#accessing popular spotify playlists w/ username and playlist id
+todays_hits = spoty.user_playlist_tracks('spotify', '37i9dQZF1DXcBWIGoYBM5M')
+us_top_fifty = spoty.user_playlist_tracks('spotify', '37i9dQZEVXbLRQDuF5jeBp')
+global_top_fifty = spoty.user_playlist_tracks('spotify', '37i9dQZEVXbMDoHDwVN2tF')
+us_viral_fifty = spoty.user_playlist_tracks('spotify', '37i9dQZEVXbKuaTI1Z1Afx')
+global_viral_fifty = spoty.user_playlist_tracks('spotify', '37i9dQZEVXbLiRSasKsNU9')
+
